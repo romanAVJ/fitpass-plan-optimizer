99a5b589352fd3713291bca0960f6744c089d4ad
Román Vélez <49596443+romanAVJ@users.noreply.github.com>
2023-08-19 17:14:17 -0600

    init commit. scrap already donde

---
+++ src/webscrap.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 18 20:00:29 2023
+
+@author: ravj
+
+Script to webscrap fitpass studios
+"""
+# %% libraries
+import pandas as pd
+import numpy as np
+import requests
+from bs4 import BeautifulSoup
+
+# 
+
+
+
+
+
+# %% main
+# URL of the website to scrape
+url = "https://www.fitpass.com/mx/mapa"
+
+# Send an HTTP GET request to the URL
+response = requests.get(url)
+print(response.text)
+
+# get the soup
+soup = BeautifulSoup(response.content)
+
+# TODO: PASS FUNCTION FROM NOTEBOOK TO HERE
