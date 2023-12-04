bd77f53d30a17446ccb845151439b001b18ea590
David Escudero <descude8@itam.mx>
2023-12-04 11:04:54 -0600

    updated Dashboardv2.0.py

--- dashboard/Dashboardv2.0.py
+++ dashboard/Dashboardv2.0.py
@@ -1,4 +1,6 @@
 import dash
+import dash_leaflet as dl
+from dash import dash_table
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
 import plotly.express as px
@@ -15,6 +17,7 @@ db_password = 'skalas-puts-me-an-aplus-in-this-class'
 database_url = f"postgresql://{db_user}:{db_password}@{db_host}/{db_name}"
 engine = sa.create_engine(database_url)
 
+
 # Inicialización de la aplicación Dash
 app = dash.Dash(__name__)
 
@@ -29,7 +32,14 @@ app.layout = html.Div([
         multi=True,
         placeholder="Selecciona actividades que te gustan"
     ),
-        dcc.Dropdown(
+    dcc.Dropdown(
+        id='input-dislike',
+        options=[{'label': activity, 'value': activity} for activity in ['barre', 'box', 'crossfit', 'cycling', 'dance', 'ems', 'functional', 'gym', 'hiit', 'mma', 'pilates', 'pool', 'running', 'sports', 'virtual_class', 'wellness', 'yoga']],
+        multi=True,
+        placeholder="Selecciona actividades que no te gustan"
+    ),
+    dcc.Input(id='input-frequency', type='number', placeholder='Número de clases al mes'),
+     dcc.Dropdown(
         id='distance-dropdown',
         options=[
             {'label': 'Cerca', 'value': 'cerca'},
@@ -39,9 +49,11 @@ app.layout = html.Div([
         placeholder="Selecciona la distancia"
     ),
     html.Button('Buscar', id='search-button', n_clicks=0),
-    dcc.Graph(id='map-view')
-
-
+    dl.Map(id='map-view', style={'width': '1000px', 'height': '500px'}, center=[19.432608, -99.133209], zoom=10, children=[
+    dl.TileLayer()
+    ]),
+    dcc.Graph(id='graph-view'),
+    html.Div(id='table-view')  # Div para mostrar la tabla de resultados
 ])
 
 # Función para calcular la distancia utilizando la fórmula de Haversine
@@ -58,12 +70,19 @@ def calculate_distance(lat1, lon1, lat2, lon2):
     distance = R * c
     return distance
 
-def generate_sql_query(lat, lon, love_activities, distance_option):
+# Función para generar la consulta SQL con las nuevas entradas
+def generate_sql_query(lat, lon, love_activities, dislike_activities, frequency, distance_option):
     query = "SELECT * FROM cdmx_studios WHERE TRUE"
 
+    # Filtrar por actividades favoritas
     if love_activities:
         love_conditions = " OR ".join([f"{activity} = 1" for activity in love_activities])
         query += f" AND ({love_conditions})"
+
+    if dislike_activities:
+        dislike_conditions = " AND ".join([f"{activity} = 0" for activity in dislike_activities])
+        query += f" AND ({dislike_conditions})"
+
 
     return query
 
@@ -73,27 +92,42 @@ def combine_dataframes(df1, df2):
     combined_df = combined_df.drop_duplicates(subset=['gym_id'])
     return combined_df
 
-# Callback para actualizar el mapa
-
 @app.callback(
-    Output('map-view', 'figure'),
-    [Input('search-button', 'n_clicks')],
-    [State('input-lat', 'value'), State('input-lon', 'value'), State('activity-dropdown', 'value'), State('distance-dropdown', 'value')]
+    [
+        Output('map-view', 'children'),  # Para los marcadores del mapa
+        Output('graph-view', 'figure'),  # Para la gráfica
+        Output('table-view', 'children')  # Para la tabla
+    ],
+    [
+        Input('search-button', 'n_clicks')
+    ],
+    [
+        State('input-lat', 'value'), State('input-lon', 'value'), 
+        State('activity-dropdown', 'value'), State('distance-dropdown', 'value'),
+        State('input-dislike', 'value'), State('input-frequency', 'value')
+    ]
 )
-def update_map(n_clicks, lat, lon, activities, distance_option):
+def update_outputs(n_clicks, lat, lon, activities, distance_option, dislikes, frequency):
+    # Validar y convertir las entradas
     try:
         lat = float(lat) if lat else None
         lon = float(lon) if lon else None
     except ValueError:
         lat, lon = None, None
-    print(f"Lat: {lat}, Lon: {lon}, Activities: {activities}, Distance: {distance_option}")
 
     if lat is not None and lon is not None:
-        query = generate_sql_query(lat, lon, activities, distance_option)
+        # Obtener estudios recomendados de la base de datos
+        query = generate_sql_query(lat, lon, activities, dislikes, frequency, distance_option)
         df_studios = pd.read_sql_query(query, engine)
-            
+        
         if n_clicks > 0 and activities:
-            sample_request = {"location": {"latitude": lat, "longitude": lon}, "love_activities": activities}
+            # Hacer la solicitud a la API
+            sample_request = {
+                "location": {"latitude": lat, "longitude": lon}, 
+                "love_activities": activities, 
+                "hate_activities": dislikes, 
+                "num_classes_per_month": frequency
+            }
             response = requests.post('http://localhost:8080/predict', json=sample_request)
             response_json = response.json()
             if isinstance(response_json, list):
@@ -105,81 +139,38 @@ def update_map(n_clicks, lat, lon, activities, distance_option):
 
             df_combined = combine_dataframes(df_studios, df_recommendations)
 
-            # Asegurarse de que las columnas 'latitude' y 'longitude' sean numéricas
+            # Preparar los datos para el mapa y la tabla
             df_combined['latitude'] = pd.to_numeric(df_combined['latitude'], errors='coerce')
             df_combined['longitude'] = pd.to_numeric(df_combined['longitude'], errors='coerce')
-
-            # Filtrar filas con datos faltantes o incorrectos en 'latitude' y 'longitude'
             df_combined = df_combined.dropna(subset=['latitude', 'longitude'])
-            print(df_combined.head())  # Imprimir las primeras filas para depuración
 
-            # Crear el mapa si hay datos válidos
+        # Crear marcadores para el mapa
+            markers = [dl.TileLayer()]
             if not df_combined.empty:
-                fig = px.scatter_mapbox(
-                    df_combined, lat='latitude', lon='longitude', hover_name='gym_name',
-                    color='gym_name', zoom=10, height=300
-                )
-                fig.update_layout(mapbox_style="open-street-map")
-                return fig
-    # Retornar un gráfico de mapa vacío si no hay datos válidos de lat y lon
-    return px.scatter_mapbox(pd.DataFrame({'lat': [], 'lon': []}), lat='lat', lon='lon')
+                for _, row in df_combined.iterrows():
+                    if pd.notnull(row['latitude']) and pd.notnull(row['longitude']):
+                        marker = dl.Marker(position=[row['latitude'], row['longitude']], children=[
+                            dl.Tooltip(row['gym_name'])
+                        ])
+                        markers.append(marker)
+
+            # Crear la gráfica de barras
+            fig_graph = px.bar(df_combined, x='gym_name', y='pro_status', color='pro_status',
+                            title='Tipo de Estudio y Fitpass Pro Status')
+
+            # Crear la tabla
+            table = dash_table.DataTable(
+                data=df_combined.to_dict('records'),
+                columns=[{"name": i, "id": i} for i in df_combined.columns],
+                style_table={'overflowX': 'scroll'}
+            )
+
+        # Asegúrate de retornar marcadores para el mapa
+        return markers, dcc.Graph(figure=fig_graph), table
+
+    # Retornar elementos vacíos si no hay datos válidos
+    return [dl.TileLayer()], html.Div(), html.Div()
 
 # Ejecución del servidor
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8051)
-
-
-
-
-
-# ... tus importaciones y configuración de la base de datos ...
-
-# Inicialización de la aplicación Dash
-app = dash.Dash(__name__)
-
-# Layout de la Aplicación
-app.layout = html.Div([
-    html.H1("Dashboard de Estudios de Fitness"),
-    # ... tus otros inputs ...
-    dcc.Input(id='input-dislike', type='text', placeholder='Actividades que no te gustan (separadas por comas)'),
-    dcc.Input(id='input-frequency', type='number', placeholder='Número de clases al mes'),
-    html.Button('Buscar', id='search-button', n_clicks=0),
-    dcc.Graph(id='map-view'),
-    html.Div(id='table-view')  # Div para mostrar la tabla de resultados
-])
-
-# Función para calcular la distancia utilizando la fórmula de Haversine
-# ... tu función calculate_distance ...
-
-# Función para generar la consulta SQL con las nuevas entradas
-def generate_sql_query(lat, lon, love_activities, dislike_activities, frequency, distance_option):
-    # ... tu lógica actual ...
-    # Agregar lógica para manejar dislike_activities y frequency si es necesario
-    return query
-
-# Función para combinar DataFrames
-# ... tu función combine_dataframes ...
-
-# Callback para actualizar el mapa y mostrar la tabla de recomendaciones
-@app.callback(
-    [Output('map-view', 'figure'), Output('table-view', 'children')],
-    [Input('search-button', 'n_clicks')],
-    [State('input-lat', 'value'), State('input-lon', 'value'), 
-     State('activity-dropdown', 'value'), State('distance-dropdown', 'value'),
-     State('input-dislike', 'value'), State('input-frequency', 'value')]
-)
-def update_outputs(n_clicks, lat, lon, activities, distance_option, dislikes, frequency):
-    # ... tu lógica actual para actualizar el mapa ...
-
-    # Aquí puedes agregar una tabla o gráfica con los estudios recomendados
-    # Por ejemplo, usando dash_table.DataTable o px.bar (para una gráfica)
-    table = dash_table.DataTable(
-        data=df_combined.to_dict('records'),
-        columns=[{"name": i, "id": i} for i in df_combined.columns]
-    )
-
-    return fig, table
-
-# Ejecución del servidor
-if __name__ == '__main__':
-    app.run_server(debug=True, port=8051)
+    app.run_server(debug=True, port=8050)
